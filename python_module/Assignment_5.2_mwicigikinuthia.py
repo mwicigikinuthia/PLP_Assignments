317baac221e03df9a096e6624552d56829be8474
mwicigikinuthia <sean.kinuthia@strathmore.edu>
2025-05-14 16:30:38 +0300

    Assignment week 5.2

---
+++ python_module/Assignment_5.2_mwicigikinuthia.py
@@ -0,0 +1,22 @@
+
+class Vehicle:
+    def move(self):
+        print("The vehicle is moving.")
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving")
+
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing")
+
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    v.move()
